617266d953449fcf7ed0e8d2dde4687e6cdf5978
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 22:33:00 -0300

    peso ideal

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿double altura,pesoIdeal;
+string sexo;
+
+Console.Write("Digite sua altura em m......: ");
+altura = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Sexo [m]asculino / [f]eminino...:");
+sexo = Console.ReadLine ()!.ToUpper().Substring(0, 1);
+
+if (sexo == "M")
+{
+    pesoIdeal = altura * 72.7 - 58.0;
+}
+else
+{
+    pesoIdeal = altura * 62.1 - 44.7;
+}
+
+Console.WriteLine($"Seu peso ideal é {pesoIdeal:N1}kg.");
